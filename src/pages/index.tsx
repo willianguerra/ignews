85de720d94648004b3b54187e46c1221cc727f87
willianGuerra <willianguerrapossamai0901@gmail.com>
2021-10-13 20:50:13 -0300

    ig.news

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+import styles from 'styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>ig.news</title>
+    </Head>
+    <h1>Hello World</h1>
+    </>
+  )
+}
