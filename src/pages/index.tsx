b76cb46a24664fcce3df01027526c4b0002aca38
willianGuerra <willianguerrapossamai0901@gmail.com>
2021-10-20 20:11:12 -0300

    included ssg in the price of the plan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Jmh0lEU3zqBh3NDyklFMMOF");
 
   const product = {
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
-  };
+    revalidate: 60 * 60 * 24, // 24h
+  }
 };
