eb1acca2a1d3b962f9932d292a5c2bd6c230ac95
willianSisplan <willian@sisplansistemas.com.br>
2021-10-20 13:18:41 -0300

    Created home screen and button component to subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
-import styles from 'styles/home.module.scss'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>ig.news</title>
+      <title>Home |ig.news</title>
     </Head>
-    <h1>Hello World</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+      <span>👏 Hey, welcome</span>
+      <h1>News about the <span>React</span> world.</h1>
+      <p>
+        Get access to all publications <br />
+        <span>for $9.90 month</span>
+      </p>
+
+      <SubscribeButton />
+
+      </section>
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      name : 'diego',
+    }
+  }
+  ;
 }
