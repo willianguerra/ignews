3cebfc19014f5c142f6a8b8f7417307d32a5c081
willianGuerra <willianguerrapossamai0901@gmail.com>
2021-10-20 19:13:30 -0300

    connected stripe and adjusted price values

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton (){
+interface SubscribeButtonsProps{
+  priceId: string;
+}
+
+export function SubscribeButton ({ priceId }: SubscribeButtonsProps){
   return (
     <button 
       type="button"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,56 @@
-import { GetServerSideProps } from 'next'
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-import styles from './home.module.scss'
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
-    <Head>
-      <title>Home |ig.news</title>
-    </Head>
-    <main className={styles.contentContainer}>
-      <section className={styles.hero}>
-      <span>👏 Hey, welcome</span>
-      <h1>News about the <span>React</span> world.</h1>
-      <p>
-        Get access to all publications <br />
-        <span>for $9.90 month</span>
-      </p>
+      <Head>
+        <title>Home |ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for {product.amount} month</span>
+          </p>
 
-      <SubscribeButton />
-
-      </section>
-      <img src="/images/avatar.svg" alt="Girl coding" />
-    </main>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1Jmh0lEU3zqBh3NDyklFMMOF");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100)
+  };
+
   return {
     props: {
-      name : 'diego',
-    }
-  }
-  ;
-}
+      product,
+    },
+  };
+};
